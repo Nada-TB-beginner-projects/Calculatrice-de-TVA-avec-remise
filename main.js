e0ed42ef8b1519758eedf7c6c2e3b64d2324674c
Nada <nada.tebba@hotmail.fr>
2019-04-24 14:57:25 +0200

    add clear function to init input value

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
-var HTT;
-const TAUX_TVA = 20.0;
-var TVA;
-var TTC;
-var remise;
-
-
+"use strict"; 
+//*********************************variables*****************************************************
 /* pour acceder à mon formulaire : document.forms["calculatrice"] ou bien document.calculatrice*/
+var HTT;
+var TTC;
+const TAUX_TVA =20;
+var TVA;
+var input1 = document.getElementById("price");
+var input2 = document.getElementById("result");
+var buttonCaclcul= document.getElementById("calcul");
+var buttonClear= document.getElementById("clear");
+var message = document.querySelector("p");
 
-/*function getHTT(){
- ;
- return HTT;
-}*/
+//********************************functions *****************************************************//
 
 function calculTTC() {
-HTT= document.calculatrice.price;
-TVA= HTT*TAUX_TVA/100;
-TTC=HTT+TVA;
-return TTC;
-document.getElementById("result").value= TTC;
+	HTT =input1.value;
+    HTT=parseFloat(HTT);
+	if (isNaN(HTT)== false && HTT !== 0) {
+	TVA=HTT*TAUX_TVA/100;
+	TTC=TVA+HTT;
+	input2.value = TTC;
+	}else{
+	
+	message.textContent="ceci n'est pas un nombre, veuillez saisir un nombre SVP";
+	}
 }
 
-/*function calculTTCRemise(){
+function clear() {
+	input1.value = "";
+	input2.value = "";
+	message.textContent="";
+}
 
-}*/
+//**************************************Events***********************************************//
 
+
+buttonCaclcul.addEventListener('click', calculTTC );
+buttonClear.addEventListener('click', clear);
